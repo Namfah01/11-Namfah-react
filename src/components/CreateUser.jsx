9e919f7d691e6e2728d6117b2cbdbcc441a6df2b
Namfah01 <kanokwanbing09@gmail.com>
2024-06-10 15:02:44 +0700

    explain code

--- src/components/CreateUser.jsx
+++ src/components/CreateUser.jsx
@@ -5,6 +5,7 @@ const CreateUser = ({ sendDataToParent }) => {
   const [name, setName] = useState("");
   const [lastname, setLastname] = useState("");
   const [position, setPosition] = useState("");
+  const [errors, setErrors] = useState({}); //ประกาศตัวแปรเพื่อ set ค่า validation
 
   //ใช้เมื่อมีการเปลี่ยนแปลงค่าของ state ของข้อมูลโดยจะส่งข้อมูลที่มีการเปลี่ยนแปลงมาในรูปแบบ event object
   const handleChange = (setter) => (e) => {
@@ -15,11 +16,26 @@ const CreateUser = ({ sendDataToParent }) => {
   const handleSubmit = (e) => {
     e.preventDefault();
     const userData = { name, lastname, position }; //เก็บข้อมูล userData ในรูปแบบ object
-    console.log("Form submitted:", userData);
-    sendDataToParent(userData); // ส่งข้อมูลผู้ใช้ไปยังหน้าหลัก
-    setName(""); //หลังจากส่งข้อมูลแล้วให้เคลียร์ค่า Name
-    setLastname(""); //หลังจากส่งข้อมูลแล้วให้เคลียร์ค่า LastName
-    setPosition(""); //หลังจากส่งข้อมูลแล้วให้เคลียร์ค่า Position
+    //สร้างตัว validation เพื่อบังคับให้ user ใส่ข้อมูลในช่องที่กำหนด
+    if (!name.trim() || !lastname.trim() || !position.trim()) {
+      //ใช้สำหรับตรวจสอบช่องว่างก่อนและหลัง ข้อความที่ input ลงไป
+      const newErrors = {}; //หากช่องinput ว่างจะกำหนดให้แสดง error
+      if (!name.trim()) {
+        newErrors.name = "Name is required";
+      }
+      if (!lastname.trim()) {
+        newErrors.lastname = "Last name is required";
+      }
+      if (!position.trim()) {
+        newErrors.position = "Position is required";
+      }
+      setErrors(newErrors);
+    } else {
+      sendDataToParent(userData); // ส่งข้อมูลผู้ใช้ไปยังหน้าหลัก
+      setName(""); //หลังจากส่งข้อมูลแล้วให้เคลียร์ค่า Name
+      setLastname(""); //หลังจากส่งข้อมูลแล้วให้เคลียร์ค่า LastName
+      setPosition(""); //หลังจากส่งข้อมูลแล้วให้เคลียร์ค่า Position
+    }
   };
 
   return (
@@ -34,18 +50,21 @@ const CreateUser = ({ sendDataToParent }) => {
             placeholder="Name"
             value={name}
             onChange={handleChange(setName)}
+            className="input1"
           />
           <input
             type="text"
             placeholder="Last Name"
             value={lastname}
             onChange={handleChange(setLastname)}
+            className="input2"
           />
           <input
             type="text"
             placeholder="Position"
             value={position}
             onChange={handleChange(setPosition)}
+            className="input3"
           />
           <button type="submit">Save</button>
         </form>
